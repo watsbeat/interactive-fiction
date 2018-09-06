a4361ad08536d2150cd50bbea840c7f41c604742
watsbeat <c.watsonbeaton@gmail.com>
2018-09-06 18:39:18 +1000

    deleted rubbish tests

--- tests/challenge_tests.rb
+++
@@ -1,6 +0,0 @@
-require_relative "challenge"
-require_relative "hangman"
-#require_relative "dictionary.txt"
-
-Hangman.play # runs game
-# changed quit to return to exit the function only, not the app itself
